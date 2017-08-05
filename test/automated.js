103f7a0d17109ee877e97b918fa7c78c76ea2720
Chris Bigelow <seebigs@gmail.com>
2017-08-05 01:13:50 -0400

    CB: --run for automated tests

--- test/automated.js
+++ test/automated.js
@@ -1,7 +1,18 @@
 const FeatherTest = require('feather-test');
+const glob = require('glob');
+const utils = require('seebigs-utils');
+
+let args = utils.args();
+let specs = './specs';
+
+if (args.run) {
+    let matcher = __dirname + '/' + specs + '/**/*' + args.run + '**';
+    specs = glob.sync(matcher);
+    console.log(specs);
+}
 
 const tests = new FeatherTest({
-    specs: './specs',
+    specs: specs,
 });
 
 tests.run();
